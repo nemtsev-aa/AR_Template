0b2b965a993bdd40643cf58677dcb45ad2d79cdb
nemtsev-aa <hiuss2bykov@gmail.com>
2023-10-24 09:24:34 +0400

    Friend

--- Assets/Scripts/DronController.cs
+++ Assets/Scripts/DronController.cs
@@ -1,16 +1,12 @@
-using TMPro;
 using UnityEngine;
 
 public class DronController : MonoBehaviour {
-    [SerializeField] private InputView _inputView;
     [SerializeField] private FixedJoystick _fixedJoystick;
     [SerializeField] private Rigidbody _rigidbody;
     
     [SerializeField] private float _speed;
     private void Start() {
         _fixedJoystick = FindObjectOfType<FixedJoystick>();
-        _inputView = FindObjectOfType<InputView>();
-        _inputView.ShowText($"Дрон создан!");
     }
 
     private void FixedUpdate() {
@@ -21,12 +17,9 @@ public class DronController : MonoBehaviour {
         _rigidbody.velocity = movement * _speed;
 
         if (xVal != 0 && yVal !=0) {
-            _inputView.ShowText($"X[{xVal:N1}]\nY[{yVal:N1}]\n{_rigidbody.velocity}]");
             transform.eulerAngles = new Vector3(transform.eulerAngles.x,
                 Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg,
                 transform.eulerAngles.z);
-        } else {
-            _inputView.ShowText($"Нет ввода!");
-        }
+        } 
     }
 }
